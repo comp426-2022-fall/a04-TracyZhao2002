87b9a47711c51f883cde68f38c57b1fba988ea58
TracyZhao2002 <tongyu0@ad.unc.edu>
2022-11-16 21:52:49 -0500

    d

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const args = minimist(process.argv.slice(2));
 
   // Setting port (#1)
-  let port = args.port || 5000;
-  if (args.port){
-    port = args.port;
-  }
+  const port = args.port || 5000;
 
 
   // Checking endpoint (#3)
@@ -23,18 +20,21 @@ const args = minimist(process.argv.slice(2));
 
   // #4 & 5
   app.get("/app/roll/", (req, res) => {
+    console.log(roll(6, 2, 1));
     res.send(roll(6, 2, 1));
   });
   app.post('/app/roll/', (req, res) => {
     const sides = parseInt(req.params.sides);
     const dice = parseInt(req.params.dice);
     const rolls = parseInt(req.params.rolls);
+    console.log(roll(sides, dice, rolls));
     res.send(roll(sides, dice, rolls));
   });
 
   // #6
   app.get('/app/roll/:sides/', (req, res) => {
     const sides = parseInt(req.params.sides);
+    console.log(roll(sides, 2, 1));
     res.send(roll(sides, 2, 1));
   });
 
@@ -42,6 +42,7 @@ const args = minimist(process.argv.slice(2));
   app.get('/app/roll/:sides/:dice/', (req, res) => {
     const sides = parseInt(req.params.sides);
     const dice = parseInt(req.params.dice);
+    console.log(roll(sides, dice, 1));
     res.send(roll(sides, dice, 1));
   });
 
@@ -50,6 +51,7 @@ const args = minimist(process.argv.slice(2));
     const sides =  parseInt(req.params.sides);
     const dice = parseInt(req.params.dice);
     const rolls = parseInt(req.params.rolls);
+    console.log(roll(sides, dice, rolls));
     res.send((roll(sides, dice, rolls)));
   });
 
@@ -58,4 +60,4 @@ const args = minimist(process.argv.slice(2));
     res.status(404).send("404 NOT FOUND");
   });
 
-
+  app.listen(port);
